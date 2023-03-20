c251b828d72a702c4a717f98a8def75a896edc04
rtdonin <62155492+rtdonin@users.noreply.github.com>
2023-03-20 15:58:49 +0000

    attempt #2

--- frontend/src/components/Item/index.js
+++ frontend/src/components/Item/index.js
@@ -36,6 +36,8 @@ class Item extends React.Component {
       return null;
     }
 
+    const placeHolderImage = "../../placeholder.png";
+
     const markup = {
       __html: marked(this.props.item.description, { sanitize: true }),
     };
@@ -48,7 +50,7 @@ class Item extends React.Component {
           <div className="row bg-white p-4">
             <div className="col-6">
               <img
-                src={this.props.item.image}
+                src={this.props.item.image  || placeHolderImage}
                 alt={this.props.item.title}
                 className="item-img"
                 style={{ height: "500px", width: "100%", borderRadius: "6px" }}
